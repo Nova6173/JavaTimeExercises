351ddd5b6236ad0d8d52562bcc1def766206db30
Jeanette S <nova777nova@hotmail.com>
2024-03-11 20:22:28 +0100

    Exercise 1 today´s date: 2024-03-11

---
+++ src/main/java/se/JavaLexicon/DateTimeExercise.java
@@ -0,0 +1,9 @@
+package se.JavaLexicon;
+import java.time.*;
+
+public class DateTimeExercise {
+    public static void main(String[] args) {
+        LocalDate today = LocalDate.now();
+        System.out.println("Today´s date: " + today);
+    }
+}
