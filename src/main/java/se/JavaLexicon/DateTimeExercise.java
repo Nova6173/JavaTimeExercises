1152d2dd34a644f0ac457745b801961322c93894
Jeanette S <nova777nova@hotmail.com>
2024-03-11 22:03:42 +0100

    Exercise 1 today´s date: 2024-03-11

--- src/main/java/se/JavaLexicon/DateTimeExercise.java
+++ src/main/java/se/JavaLexicon/DateTimeExercise.java
@@ -1,9 +1,17 @@
 package se.JavaLexicon;
+
 import java.time.*;
+import java.time.format.DateTimeFormatter;
 
 public class DateTimeExercise {
     public static void main(String[] args) {
         LocalDate today = LocalDate.now();
         System.out.println("Today´s date: " + today);
+
+        LocalDate currentDate = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEEE d MMMM");
+        String formattedDate = currentDate.format(formatter);
+        System.out.println(formattedDate);
     }
+
 }
