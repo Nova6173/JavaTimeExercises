8b5152a3d20e34409e9eed13d7f0b8aee5bf9b78
Jeanette S <nova777nova@hotmail.com>
2024-03-11 23:07:12 +0100

    Exercise 3; Last Måndag

--- src/main/java/se/JavaLexicon/DateTimeExercise.java
+++ src/main/java/se/JavaLexicon/DateTimeExercise.java
@@ -2,6 +2,8 @@ package se.JavaLexicon;
 
 import java.time.*;
 import java.time.format.DateTimeFormatter;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
 
 public class DateTimeExercise {
     public static void main(String[] args) {
@@ -12,6 +14,13 @@ public class DateTimeExercise {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEEE d MMMM");
         String formattedDate = currentDate.format(formatter);
         System.out.println(formattedDate);
+
+        LocalDate date = LocalDate.now().with(DayOfWeek.MONDAY).minusWeeks(1);
+
+         for (int i = 0; i < 7; i++){
+             System.out.println(date);
+             date = date.plusDays(1);
+         }
     }
 
 }
